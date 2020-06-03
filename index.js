e6be2fc2434eba14b55910c9f9296f49d28a478f
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-06-03 10:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, event) {
+  let array = []
+  for (let i = 0; i < namesArray.length ; i++) {
+    array.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return array;
+}
+
+function countDown(number){
+  for (let i = number; i >= 0; i--){
+    console.log(i);
+  }
+}
