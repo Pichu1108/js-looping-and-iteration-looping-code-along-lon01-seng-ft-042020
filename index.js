90bf07d0c02e3b071e7d90f791b4167231184715
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-06-03 09:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const friends = [ 'Lisa', 'Kaitlin', 'Jan' ];
 
 function writeCards(nameOfArray, event) {
   for (let i = 0; i < nameOfArray.length ; i++) {
-    console.log(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+    console.log(`Thank you, ${nameOfArray[i]}, for the wonderful surprise gift!`);
   }
 
   return friends;
